2db04b73d58a01c87208b64ec62121ab3d6650fd
Richard S. Monfort <richard.s.monfort@gmail.com>
2014-09-27 14:16:56 -0400

    Added source and example input file

---
+++ Fibonacci_Series/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Checks args for path to input file, otherwise ask user
+            string[] lines;
+            if (args.Length > 0)
+            {
+                lines = System.IO.File.ReadAllLines(args[0]);
+            }
+            else
+            {
+                Console.WriteLine("Please enter the path to your input file:");
+                string input_file = Console.ReadLine();
+                lines = System.IO.File.ReadAllLines(@input_file);
+            }
+            foreach (var line in lines)
+            {
+                // Ignore white space and negative numbers
+                if (line == "" || Convert.ToInt32(line) < 0)
+                {
+                    continue;
+                }
+                int number = Convert.ToInt32(line);
+                Console.WriteLine(Fibonacci(ref number));
+            }
+        }
+
+        // Calculates the nth Fibonacci number using Binet's Fibonacci number formula
+        static public int Fibonacci(ref int n)
+        {
+            double sqrt_of_5 = Math.Sqrt(5);
+            double numerator = Math.Pow((1 + sqrt_of_5), n) - Math.Pow((1 - sqrt_of_5), n);
+            double denominator = Math.Pow(2, n) * sqrt_of_5;
+            double Fibonacci_number = numerator / denominator;
+            return (int)Fibonacci_number;
+        }
+    }
+}
